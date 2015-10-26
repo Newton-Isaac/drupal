375dd64f1628811a394e5339505f43acaffda7d9
dominik <dominik@dominikbook.studio.local>
2015-10-26 13:57:10 +0100

    Initial  release

---
+++ sites/default/files/php/twig/ef44ece8_form.html.twig_e331c0222bf226e82a1c3068c4ac5a62b7c398b273b3bb43d2fb2ec8fb22050c/191279a7ae953c47f948b129232c9bf0f0721391f92c82e47977af0ea6b817d9.php
@@ -0,0 +1,60 @@
+<?php
+
+/* core/themes/classy/templates/form/form.html.twig */
+class __TwigTemplate_e27d396b5d923ebec1ada33087dc7d55a534110daff6a0e7fd4e91ee83a7f520 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 13
+        echo "<form";
+        echo $this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["attributes"]) ? $context["attributes"] : null), "html", null, true);
+        echo ">
+  ";
+        // line 14
+        echo $this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["children"]) ? $context["children"] : null), "html", null, true);
+        echo "
+</form>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/form/form.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  24 => 14,  19 => 13,);
+    }
+}
+/* {#*/
+/* /***/
+/*  * @file*/
+/*  * Theme override for a 'form' element.*/
+/*  **/
+/*  * Available variables*/
+/*  * - attributes: A list of HTML attributes for the wrapper element.*/
+/*  * - children: The child elements of the form.*/
+/*  **/
+/*  * @see template_preprocess_form()*/
+/*  *//* */
+/* #}*/
+/* <form{{ attributes }}>*/
+/*   {{ children }}*/
+/* </form>*/
+/* */
